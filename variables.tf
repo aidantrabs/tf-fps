777724738b990f6bb0a03dd631f05fa5e216ffed
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-01-15 11:54:46 -0400

    feat: add allowed cidr blocks + app port from tfvars

--- variables.tf
+++ variables.tf
@@ -15,3 +15,16 @@ variable "environment" {
     description = "Environment name"
     default     = "dev"
 }
+
+# loaded from terraform.tfvars (sensitive)
+variable "allowed_cidr_blocks" {
+    type        = list(string)
+    description = "List of CIDR blocks allowed to access Faster Payments MGS"
+    sensitive   = true
+}
+
+variable "fp_app_port" {
+    type        = number
+    description = "Port for Faster Payments application"
+    default     = 7297
+}
