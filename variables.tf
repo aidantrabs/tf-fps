82380eb7b209f07e3488e5b0fcc4e64f1888cc95
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-01-15 09:18:35 -0400

    feat: add vpc cidr var + env var

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
     description = "AWS region"
     default     = "us-east-1"
 }
+
+variable "vpc_cidr" {
+    type        = string
+    description = "VPC CIDR block"
+    default     = "10.0.0.0/16"
+}
+
+variable "environment" {
+    type        = string
+    description = "Environment name"
+    default     = "dev"
+}
