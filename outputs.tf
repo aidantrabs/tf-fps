e42a0372aa8f82f1594c6936946001f2562e41b2
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-01-15 11:54:17 -0400

    feat: add vpc_id output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
+output "vpc_id" {
+    description = "ID of the VPC"
+    value       = module.vpc.vpc_id
+}
+
 output "security_group_id" {
     description = "ID of the Faster Payments MGS security group"
-    value       = aws_security_group.faster_payments_mgs.id
+    value       = module.faster_payments_sg.security_group_id
 }
